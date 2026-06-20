b2576367e70438d07238cd4006c8488bb3176aa9
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-06-20 13:57:49 +0900

    feat: Armament 인디케이터 색상을 위한 IndicatorColor Enum 추가 및 적용

--- app/src/main/java/com/example/gundamdexapp/feature/detail/model/Armament.kt
+++ app/src/main/java/com/example/gundamdexapp/feature/detail/model/Armament.kt
@@ -3,5 +3,5 @@ package com.example.gundamdexapp.feature.detail.model
 data class Armament(
     val name: String = "",
     val details: String = "",
-    val indicatorColor: String = ""
+    val indicatorColor: IndicatorColor = IndicatorColor.RED
 )
