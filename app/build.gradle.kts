811c20292241e122db728a150acaee17f36155e9
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-06-25 13:39:33 +0900

    chore: activity-ktx 의존성 및 버전 설정 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.activity.ktx)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
